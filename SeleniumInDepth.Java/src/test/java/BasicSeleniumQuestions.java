0c006502fa28b52fdb553cb8a16ced52610fd4b0
arnonax <arnonaxelrod@hotmail.com>
2017-10-08 13:41:53 +0300

    Updated Java version of SimpleDemo

--- SeleniumInDepth.Java/src/test/java/BasicSeleniumQuestions.java
+++ SeleniumInDepth.Java/src/test/java/BasicSeleniumQuestions.java
@@ -25,69 +25,37 @@ public class BasicSeleniumQuestions {
     public static final String BASE_URL = "https://SeleniumInDepthDemos.AzureWebSites.net/";
     // region plumbing code
 
-    private WebDriver _driver;
+    private WebDriver driver;
 
     @Before
     public void TestInitialize()
     {
         System.setProperty("webdriver.chrome.driver","c:\\temp\\chromeDriver.exe");
-        _driver = new ChromeDriver();
+        driver = new ChromeDriver();
     }
 
     @After
     public void TestCleanup()
     {
-        _driver.quit();
+        driver.quit();
     }
 
     // endregion
 
     @Test
-    public void IsItABugInSelenium()
+    public void SimpleDemo()
     {
-        _driver.navigate().to(BASE_URL + "IsItABugInSelenium.html");
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        WebElement button = _driver.findElement(By.id("myButton"));
-        button.click();
-    }
+        String page = "SimpleDemo.html";
+        //String page = "SimpleDemo.1.html";
+        //String page = "SimpleDemo.2.html";
+        //String page = "SimpleDemo.3.html";
+        driver.navigate().to(BASE_URL + page);
 
-    @Test
-    public void DoWeReallyNeedWait()
-    {
-        _driver.navigate().to(BASE_URL + "SlowButton.html");
+        WebElement button = driver.findElement(By.id("myButton"));
+        WebElement input = driver.findElement(By.id("myInput"));
 
-        WebElement button = _driver.findElement(By.id("myButton"));
-        WebElement input = _driver.findElement(By.id("myInput"));
-
-        // Click and immidiately assert!
-        button.click();
-        Assert.assertEquals("Done", input.getAttribute("value"));
-    }
-
-    @Test
-    public void WhatsTheDifference()
-    {
-        _driver.navigate().to(BASE_URL + "SlowButton.1.html");
-
-        WebElement button = _driver.findElement(By.id("myButton"));
-        WebElement input = _driver.findElement(By.id("myInput"));
-
-        // Click and immidiately assert!
-        button.click();
-        Assert.assertEquals("Done", input.getAttribute("value"));
-    }
-
-    @Test
-    public void WillThisHelp()
-    {
-        _driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        _driver.navigate().to(BASE_URL + "SlowButton.1.html");
-
-        WebElement button = _driver.findElement(By.id("myButton"));
-        WebElement input = _driver.findElement(By.id("myInput"));
-
-        // Click and immidiately assert!
         button.click();
         Assert.assertEquals("Done", input.getAttribute("value"));
     }
@@ -95,13 +63,13 @@ public class BasicSeleniumQuestions {
     @Test
     public void ExplicitWait()
     {
-        _driver.navigate().to(BASE_URL + "SlowButton.1.html");
+        driver.navigate().to(BASE_URL + "SlowButton.1.html");
 
-        WebElement button = _driver.findElement(By.id("myButton"));
-        final WebElement input = _driver.findElement(By.id("myInput"));
+        WebElement button = driver.findElement(By.id("myButton"));
+        final WebElement input = driver.findElement(By.id("myInput"));
 
         button.click();
-        WebDriverWait wait = new WebDriverWait(_driver, 10);     // change to 1 second to see it fail
+        WebDriverWait wait = new WebDriverWait(driver, 10);     // change to 1 second to see it fail
         wait.until(ExpectedConditions.textToBePresentInElementValue(input, "Done"));
         Assert.assertEquals("Done", input.getAttribute("value"));
     }
@@ -114,7 +82,7 @@ public class BasicSeleniumQuestions {
         //noinspection ResultOfMethodCallIgnored
         file.delete();
 
-        WebDriverWait wait = new WebDriverWait(_driver, 60);
+        WebDriverWait wait = new WebDriverWait(driver, 60);
         wait.ignoring(IOException.class);
         String content = wait.until(new ExpectedCondition<String>() {
             public String apply(WebDriver drv) {
@@ -133,9 +101,9 @@ public class BasicSeleniumQuestions {
     @Test // Which line throws the exception?
     public void ObviousNoSuchElementException()
     {
-        _driver.navigate().to(BASE_URL + "DummyPage.html");
+        driver.navigate().to(BASE_URL + "DummyPage.html");
 
-        WebElement button = _driver.findElement(By.id("NonExistentId")); // (a)
+        WebElement button = driver.findElement(By.id("NonExistentId")); // (a)
 
         button.click(); // (b)
     }
@@ -143,24 +111,24 @@ public class BasicSeleniumQuestions {
     @Test
     public void WhenImplicitlyWaitHelps()
     {
-        _driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        _driver.navigate().to(BASE_URL + "DynamicElement.html");
-        WebElement button = _driver.findElement(By.id("myButton"));
+        driver.navigate().to(BASE_URL + "DynamicElement.html");
+        WebElement button = driver.findElement(By.id("myButton"));
         button.click();
 
         // This element didn't exist before the click!
-        WebElement input = _driver.findElement(By.id("myInput"));
+        WebElement input = driver.findElement(By.id("myInput"));
         Assert.assertEquals("Done", input.getAttribute("value"));
     }
 
     @Test
     public void StaleReferenceElementDemo()
     {
-        _driver.navigate().to(BASE_URL + "RemoveElement.html");
+        driver.navigate().to(BASE_URL + "RemoveElement.html");
         // Find both elements first - Should succeed!
-        WebElement button = _driver.findElement(By.id("myButton"));
-        WebElement input = _driver.findElement(By.id("myInput"));
+        WebElement button = driver.findElement(By.id("myButton"));
+        WebElement input = driver.findElement(By.id("myInput"));
 
         button.click();
 
@@ -174,9 +142,9 @@ public class BasicSeleniumQuestions {
     @Test
     public void UnjustifiedStaleElement()
     {
-        _driver.navigate().to(BASE_URL + "SeemlessStaleElement.html");
-        WebElement button = _driver.findElement(By.id("myButton"));
-        WebElement input = _driver.findElement(By.id("myInput"));
+        driver.navigate().to(BASE_URL + "SeemlessStaleElement.html");
+        WebElement button = driver.findElement(By.id("myButton"));
+        WebElement input = driver.findElement(By.id("myInput"));
 
         button.click();
 
@@ -188,8 +156,8 @@ public class BasicSeleniumQuestions {
     @Test
     public void WhatHappensWhenfindElementsFindsNothing()
     {
-        _driver.navigate().to("http://www.google.com");
-        List<WebElement> elements = _driver.findElements(By.className("non-exitent-class-name"));
+        driver.navigate().to("http://www.google.com");
+        List<WebElement> elements = driver.findElements(By.className("non-exitent-class-name"));
         // What will happen?
         // a. NoSuchElementException will be throw
         // b. elements will be null
@@ -201,18 +169,18 @@ public class BasicSeleniumQuestions {
     @Test
     public void findElementsAndExplicitlyWait()
     {
-        _driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        _driver.navigate().to("http://www.google.com");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.navigate().to("http://www.google.com");
         List<WebElement> elements;
 
         System.out.println("findElements(By.className(\"gb_P\")) started at: " + new Date());
-        elements = _driver.findElements(By.className("gb_P"));
+        elements = driver.findElements(By.className("gb_P"));
         System.out.println("findElements(By.className(\"gb_P\")) returned at: " + new Date());
         System.out.println("Elements found: " + elements.size());
         System.out.println();
 
         System.out.println("findElements(By.id(\"non-exitent-id\")) started at: " + new Date());
-        elements = _driver.findElements(By.id("non-exitent-id"));
+        elements = driver.findElements(By.id("non-exitent-id"));
         System.out.println("findElements(By.id(\"non-exitent-id\")) returned at: " + new Date());
         System.out.println("Elements found: " + elements.size());
     }
@@ -220,15 +188,15 @@ public class BasicSeleniumQuestions {
     @Test
     public void CalculateTest()
     {
-        _driver.navigate().to(BASE_URL + "Calculate.html");
-        WebElement x = _driver.findElement(By.id("X"));
+        driver.navigate().to(BASE_URL + "Calculate.html");
+        WebElement x = driver.findElement(By.id("X"));
         x.sendKeys("5");
-        WebElement y = _driver.findElement(By.id("Y"));
+        WebElement y = driver.findElement(By.id("Y"));
         y.sendKeys("3");
-        WebElement button = _driver.findElement(By.tagName("button"));
+        WebElement button = driver.findElement(By.tagName("button"));
         button.click();
 
-        String result = _driver.findElement(By.id("calculationResult")).getText();
+        String result = driver.findElement(By.id("calculationResult")).getText();
         Assert.assertEquals("8", result);
 
         // If this would happen only occasionally, who would you blame?
